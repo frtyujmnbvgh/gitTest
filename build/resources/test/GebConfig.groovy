8a22868cc4ca8ccd8aa06afd1c98d001f8880fa5
Yutaka Kumagai <yutaka_kumagai@bsp.co.jp>
2013-11-12 09:42:10 +0900

    commit2
    commiting

---
+++ build/resources/test/GebConfig.groovy
@@ -0,0 +1,36 @@
+/*
+	This is the Geb configuration file.
+
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
+waiting {
+	timeout = 3
+}
+
+environments {
+
+	// run via “./gradlew chromeTest”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chrome {
+		driver = { new ChromeDriver() }
+	}
+
+	// run via “./gradlew firefoxTest”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+	firefox {
+		driver = { new FirefoxDriver() }
+	}
+
+	htmlunit {
+		driver = { new HtmlUnitDriver() }
+	}
+
+}
+
+// To run the tests with all browsers just run “./gradlew test”
