3fd478efcdf14d8f313e88d859f9638f0d12afd2
Yutaka Kumagai <yutaka_kumagai@bsp.co.jp>
2013-11-12 09:41:39 +0900

    Revert "commit"
    This reverts commit b1e1606cfbe6d7091ad8cf31a947e36941dbdc96.

--- src/test/groovy/GoogleHomePage.groovy
+++
@@ -1,9 +0,0 @@
-import geb.*
-
-class GoogleHomePage extends Page {
-    static url = "http://google.com/ncr"
-    static at = { title == "Google" }
-	static content = {
-		input { $("form#gbqf") }
-	}
-}
